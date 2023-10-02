1fc02dac0c6278691a41364d474e10fb32cbfc9c
Dokotela <grey.faulkenberry@mayjuun.com>
2023-10-02 13:28:14 -0400

    Adding test examples

---
+++ lib/cql/main.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+Future<void> main() async {
+  final librariesDir = Directory('cooking_with_cql');
+  final librariesFiles = librariesDir.listSync();
+  for (final file in librariesFiles) {
+    if (file is File) {
+      print(file.path);
+      final pathExpression = await file.readAsString();
+      final pathExpressionLists = pathExpression.split('\n');
+      final librariesList = <String>[];
+      for (final line in pathExpressionLists) {
+        librariesList.add(line);
+        if (line.startsWith('library')) {
+          librariesList.add(line);
+          await File(file.path.replaceAll('cooking_with_cql', 'libraries'))
+              .writeAsString(librariesList.join('\n'));
+        }
+      }
+    }
+  }
+}
